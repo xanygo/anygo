8d372aa469d13bd6e75576b11ef5a83bc5ad4824
hidu <duv123+git@gmail.com>
2024-09-04 20:59:21 +0800

    update

--- anygo.go
+++ anygo.go
@@ -11,3 +11,33 @@ func Ternary[T any](cond bool, trueValue T, falseValue T) T {
 	}
 	return falseValue
 }
+
+// Must 若 values 中任意一项是 error 并且 err != nil ，则 panic
+func Must(values ...any) {
+	for _, v := range values {
+		if err, ok := v.(error); ok && err != nil {
+			panic(err)
+		}
+	}
+}
+
+type Then struct {
+	err error
+}
+
+func (t *Then) Then(fn func() error) *Then {
+	if t.err == nil {
+		t.err = fn()
+	}
+	return t
+}
+
+func (t *Then) Err() error {
+	return t.err
+}
+
+func DoThen(fn func() error) *Then {
+	return &Then{
+		err: fn(),
+	}
+}
--- example_test.go
+++ example_test.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	"github.com/xanygo/anygo"
+	"io"
 )
 
 func ExampleTernary() {
@@ -18,4 +19,43 @@ func ExampleTernary() {
 	// Output:
 	// v1
 	// v2
+}
+
+func ExampleMust() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("panic:", err)
+		}
+	}()
+	run := func() (int, error) {
+		return 1, io.EOF
+	}
+	anygo.Must(run())
+
+	// Output:
+	// panic: EOF
+}
+
+func ExampleDo() {
+	var called int
+	err := anygo.DoThen(func() error {
+		called += 1
+		// do something
+		return nil
+	}).Then(func() error {
+		called += 3
+		// do something
+		return io.EOF
+	}).Then(func() error {
+		// 由于前面 err != nil，所以此处的方法不会执行
+		called += 5
+		return nil
+	}).Err()
+
+	fmt.Println("called=", called)
+	fmt.Println("err=", err)
+
+	// Output:
+	// called= 4
+	// err= EOF
 }
