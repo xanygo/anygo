78d4de26abcc7e9095ee0a93d51b3989a950338c
hidu <duv123+git@gmail.com>
2024-10-18 19:45:30 +0800

    update

--- xmap/map.go
+++ xmap/map.go
@@ -6,7 +6,7 @@ package xmap
 
 // Get 从 map 中读取指定 key 的值。支持 map 为 nil。
 func Get[Map ~map[K]V, K comparable, V any](m Map, key K) (v V, found bool) {
-	if m == nil {
+	if len(m) == 0 {
 		return v, false
 	}
 	v, found = m[key]
@@ -15,7 +15,7 @@ func Get[Map ~map[K]V, K comparable, V any](m Map, key K) (v V, found bool) {
 
 // GetDf  从 map 中读取指定 key 的值,若 key 不存在则返回默认值。支持 map 为 nil。
 func GetDf[Map ~map[K]V, K comparable, V any](m Map, key K, def V) V {
-	if m == nil {
+	if len(m) == 0 {
 		return def
 	}
 	v, found := m[key]
@@ -27,20 +27,50 @@ func GetDf[Map ~map[K]V, K comparable, V any](m Map, key K, def V) V {
 
 // HasKey 判断 map 中是否存在特定 key。支持 map 为 nil。
 func HasKey[Map ~map[K]V, K comparable, V any](m Map, key K) bool {
-	if m == nil {
+	if len(m) == 0 {
 		return false
 	}
 	_, found := m[key]
 	return found
 }
 
+// HasAnyKey 判断 map 中是否存在任意 key。支持 map 为 nil。
+// 若 map 或者 keys 为空，均会返回 false
+func HasAnyKey[Map ~map[K]V, K comparable, V any](m Map, keys ...K) bool {
+	if len(m) == 0 || len(keys) == 0 {
+		return false
+	}
+	for _, key := range keys {
+		if _, found := m[key]; found {
+			return true
+		}
+	}
+
+	return false
+}
+
 // HasKeyValue 判断 map 中是否有指定的 key 和 value。支持 map 为 nil。
 func HasKeyValue[Map ~map[K]V, K comparable, V comparable](m Map, key K, value V) bool {
-	if m == nil {
+	if len(m) == 0 {
 		return false
 	}
 	val, found := m[key]
 	return found && val == value
+}
+
+// HasAnyKeyValue 查找 map 中是否有 search 中的任意一项 key-value 。
+// 若 m 或 search 为空，均会返回 false
+func HasAnyKeyValue[Map ~map[K]V, K comparable, V comparable](m Map, search Map) bool {
+	if len(m) == 0 || len(search) == 0 {
+		return false
+	}
+	for key, value := range search {
+		val, found := m[key]
+		if found && val == value {
+			return true
+		}
+	}
+	return false
 }
 
 // Keys 返回 map 所有的 key。支持 map 为 nil。
@@ -77,6 +107,42 @@ func Filter[Map ~map[K]V, K comparable, V any](m Map, filter func(k K, v V, ok i
 	}
 	for k, v := range m {
 		if filter(k, v, len(result)) {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+// FilterByKeys 从 map 中筛选出指定的 key 的项
+func FilterByKeys[Map ~map[K]V, K comparable, V any](m Map, keys ...K) Map {
+	result := make(Map)
+	if len(m) == 0 || len(keys) == 0 {
+		return result
+	}
+	km := make(map[K]struct{}, len(keys))
+	for _, k := range keys {
+		km[k] = struct{}{}
+	}
+	for k, v := range m {
+		if _, ok := km[k]; ok {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+// FilterByValues 从 map 中筛选出指定的 key 的项
+func FilterByValues[Map ~map[K]V, K comparable, V comparable](m Map, values ...V) Map {
+	result := make(Map)
+	if len(m) == 0 || len(values) == 0 {
+		return result
+	}
+	vm := make(map[V]struct{}, len(values))
+	for _, v := range values {
+		vm[v] = struct{}{}
+	}
+	for k, v := range m {
+		if _, ok := vm[v]; ok {
 			result[k] = v
 		}
 	}
